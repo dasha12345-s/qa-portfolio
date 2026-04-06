dd2cc5a73b06d137fe29d95f8a14f4f3e146fe6f
Darya Shostak <shostak19@icloud.com>
2026-04-06 13:43:43 -0400

    new project added

--- saucedemo-full-suite/cypress/support/commands.js
+++ saucedemo-full-suite/cypress/support/commands.js
@@ -1,6 +1,7 @@
 Cypress.Commands.add('login', (userType = 'standard') => {
     cy.fixture('users').then(users => {
         const user = users[userType]
+        
         cy.visit('/')
         cy.get('[data-test="username"]').type(user.username)
         cy.get('[data-test="password"]').type(user.password)
