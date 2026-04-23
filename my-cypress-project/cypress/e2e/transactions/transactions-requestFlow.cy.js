137fc76f0ee59537c7a044f6eec0b4433cd13340
Darya Shostak <shostak19@icloud.com>
2026-04-23 15:45:25 -0400

    transaction page - rejection flow added. FOUND a BUG

--- my-cypress-project/cypress/e2e/transactions/transactions-requestFlow.cy.js
+++ my-cypress-project/cypress/e2e/transactions/transactions-requestFlow.cy.js
@@ -17,11 +17,11 @@ describe('TransactionPage - request payment ', () => {
         cy.get('@userId').then((id) => {
             cy.request('GET', `http://localhost:3001/users/${id}`).then((response) => {
                 expect(response.status).to.eq(200);
-                cy.wrap(response.body.user.balance).as('apiBalanceFirstUser');
+                cy.wrap(response.body.user.balance).as('apiBalanceFirstUser');//150953
             })
         })
 
-        //REQUEST from user1: make payment and capture transaction data
+        //REQUEST from user1: make request and capture transaction data
         cy.intercept('POST', '**/transactions').as('NewRequest')
 
         newTransactionPage.newTransactionButton.click()
@@ -33,7 +33,7 @@ describe('TransactionPage - request payment ', () => {
         });
 
         newTransactionPage.amount.click().type(5)
-        newTransactionPage.noteField.type('accept money!')
+        newTransactionPage.noteField.type('money request!')
         newTransactionPage.request.click()
 
         cy.wait('@NewRequest').then(interception => {
@@ -45,10 +45,12 @@ describe('TransactionPage - request payment ', () => {
 
         cy.url().should('include', '/signin')
 
+        //SETUP: capture sender data BEFORE transaction accepted 
         cy.login('alternativeUser')
 
-         cy.request('GET','http://localhost:3001/checkAuth').then((response) => {
-        cy.wrap(response.body.user.balance).as('balanceBeforeAccept');}) //-- I STOPPED HERE
+        cy.request('GET', 'http://localhost:3001/checkAuth').then((response) => {
+            cy.wrap(response.body.user.balance).as('balanceBeforeAccept');
+        })//158880
 
         cy.get('@TransactionId').then((id) => {
             homePage.transactionList.within(() => {
@@ -56,7 +58,39 @@ describe('TransactionPage - request payment ', () => {
             })
         })
 
+        //ACTION: Transaction accepted 
+
         transactionDetailPage.acceptButton.click()
+
+        //SETUP: capture sender data AFTER transaction accepted
+        cy.request('GET', 'http://localhost:3001/checkAuth').then((response) => {
+            cy.wrap(response.body.user.balance).as('balanceAfterAccept');
+        }) //158380
+
+        //ACTION: comparing BEFORE AND AFTER balance
+        cy.get('@balanceBeforeAccept').then((balanceBefore) => {
+            cy.get('@balanceAfterAccept').then((balanceAfter) => {
+                expect(balanceAfter).to.eq(balanceBefore - 500)
+            })
+        })
+
+        sideBarMenu.logout()
+
+        cy.url().should('include', '/signin')
+
+        cy.login('defaultUser')
+
+        //SETUP: capture receiver data AFTER transaction accepted
+        cy.request('GET', 'http://localhost:3001/checkAuth').then((response) => {
+            cy.wrap(response.body.user.balance).as('firstUserBalanceAfterAccept');
+        })
+
+        //ACTION: comparing BEFORE AND AFTER balance
+        cy.get('@apiBalanceFirstUser').then((balanceBeforeFirstUser) => {
+            cy.get('@firstUserBalanceAfterAccept').then((balanceAfterFirstUser) => {
+                expect(balanceAfterFirstUser).to.eq(balanceBeforeFirstUser + 500)
+            })
+        })
     })
 
     it('should reject a payment request and verify both balances are the same', () => {
@@ -67,11 +101,11 @@ describe('TransactionPage - request payment ', () => {
         cy.get('@userId').then((id) => {
             cy.request('GET', `http://localhost:3001/users/${id}`).then((response) => {
                 expect(response.status).to.eq(200);
-                cy.wrap(response.body.user.balance).as('apiBalanceFirstUser');
+                cy.wrap(response.body.user.balance).as('apiBalanceFirstUser');//150953
             })
         })
 
-        //REQUEST from user1: make payment and capture transaction data
+        //REQUEST from user1: make request and capture transaction data
         cy.intercept('POST', '**/transactions').as('NewRequest')
 
         newTransactionPage.newTransactionButton.click()
@@ -83,7 +117,7 @@ describe('TransactionPage - request payment ', () => {
         });
 
         newTransactionPage.amount.click().type(50)
-        newTransactionPage.noteField.type('reject money!')
+        newTransactionPage.noteField.type('reject request!')
         newTransactionPage.request.click()
 
         cy.wait('@NewRequest').then(interception => {
@@ -95,7 +129,12 @@ describe('TransactionPage - request payment ', () => {
 
         cy.url().should('include', '/signin')
 
+        //SETUP: capture sender data BEFORE transaction rejected
         cy.login('alternativeUser')
+
+        cy.request('GET', 'http://localhost:3001/checkAuth').then((response) => {
+            cy.wrap(response.body.user.balance).as('balanceBeforeReject');//158880
+        })
 
         cy.get('@TransactionId').then((id) => {
             homePage.transactionList.within(() => {
@@ -103,6 +142,37 @@ describe('TransactionPage - request payment ', () => {
             })
         })
 
+        //ACTION: Transaction rejected 
+        transactionDetailPage.rejectButton.click()
 
+        //SETUP: capture sender data AFTER transaction rejected
+        cy.request('GET', 'http://localhost:3001/checkAuth').then((response) => {
+            cy.wrap(response.body.user.balance).as('balanceAfterReject');//153880????? BUG
+        })
+
+         //ACTION: comparing BEFORE AND AFTER balance
+        cy.get('@balanceBeforeReject').then((balanceBefore) => {
+            cy.get('@balanceAfterReject').then((balanceAfter) => {
+                expect(balanceAfter).to.eq(balanceBefore)
+            })
+        })
+
+        sideBarMenu.logout()
+
+        cy.url().should('include', '/signin')
+
+        cy.login('defaultUser')
+
+        //SETUP: capture receiver data AFTER transaction rejected
+        cy.request('GET', 'http://localhost:3001/checkAuth').then((response) => {
+            cy.wrap(response.body.user.balance).as('firstUserBalanceAfterReject');
+        })
+
+        //ACTION: comparing BEFORE AND AFTER balance 
+        cy.get('@apiBalanceFirstUser').then((balanceBeforeFirstUser) => {
+            cy.get('@firstUserBalanceAfterReject').then((balanceAfterFirstUser) => {
+                expect(balanceAfterFirstUser).to.eq(balanceBeforeFirstUser)
+            })
+        })
     })
 })
