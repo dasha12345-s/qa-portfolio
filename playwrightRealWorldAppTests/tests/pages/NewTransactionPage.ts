e1abfe0a13dc88240d5f3e455bfea32513073f8f
Darya Shostak <shostak19@icloud.com>
2026-05-24 10:54:53 -0400

    newTransaction tests added

--- playwrightRealWorldAppTests/tests/pages/NewTransactionPage.ts
+++ playwrightRealWorldAppTests/tests/pages/NewTransactionPage.ts
@@ -10,6 +10,7 @@ export class NewTransactionPage {
 
 
     get searchInput() {return this.page.getByPlaceholder('Search')}
+    get userListItem() {return this.page.locator('[data-test^="user-list-item-"]')}
     get amountInput() {return this.page.getByPlaceholder('Amount')}
     get noteInput() {return this.page.getByPlaceholder('Add a note')}
     get requestButton() {return this.page.getByRole('button', { name: 'Request' })}
@@ -17,7 +18,7 @@ export class NewTransactionPage {
     get errorMessageAmountRequired() { return this.page.getByText('Please enter a valid amount') }
     get errorMessageAmountPositive() { return this.page.getByText('amount must be') }
     get errorMessageNoteRequired() { return this.page.getByText('Please enter a note') }
-    get returnToTransactionsButton() { return this.page.getByRole('button', { name: 'Return to transactions' }) }
+    get returnToTransactionsButton() { return this.page.locator('[data-test="new-transaction-return-to-transactions"]') }
     get createAnotherTransactionButton() { return this.page.getByRole('button', { name: 'Create another transaction' }) }   
 
     async goto() {
