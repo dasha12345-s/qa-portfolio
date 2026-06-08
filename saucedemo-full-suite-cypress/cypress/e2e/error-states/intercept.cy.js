3c45b182b9f8ad2674a5697d842851b430509363
Darya Shostak <shostak19@icloud.com>
2026-06-08 09:53:08 -0400

    small changes

--- saucedemo-full-suite-cypress/cypress/e2e/error-states/intercept.cy.js
+++ saucedemo-full-suite-cypress/cypress/e2e/error-states/intercept.cy.js
@@ -31,18 +31,4 @@ describe('cy.intercept - error state testing', () => {
             expect(interception.response.body.email).to.eq('darya@test.com')
         })
     })
-
-    it('should spy on a real API call without stubbing it', () => {
-        cy.intercept('GET', 'https://jsonplaceholder.typicode.com/users/1').as('spyUser')
-
-        cy.window().then(win => {
-            win.fetch('https://jsonplaceholder.typicode.com/users/1')
-        })
-
-        cy.wait('@spyUser').then(interception => {
-            expect(interception.response.statusCode).to.eq(200)
-            expect(interception.response.body.id).to.eq(1)
-            expect(interception.response.body.name).to.eq('Leanne Graham')
-        })
-    })
 })
